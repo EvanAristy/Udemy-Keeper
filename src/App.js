c0301b01aeae4779db6b38c2a2eeea1d57209666
EvanAristy <evanjaristy@gmail.com>
2022-04-25 18:43:22 -0400

    mapping through notes array to render everything

--- src/App.js
+++ src/App.js
@@ -4,12 +4,21 @@ import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Note from './components/Note';
+import notes from './notes';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Note />
+      {
+        notes.map(note => (
+          <Note
+            key = {note.key}
+            title = {note.title}
+            content = {note.content}
+          />
+        ))
+      }
       <Footer />
     </div>
   );
--- src/components/Note/index.jsx
+++ src/components/Note/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const Note = () => {
+const Note = (props) => {
     return (
         <div className='note'>
-            <h1>Note Title</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla laudantium obcaecati mollitia praesentium amet optio unde, neque laboriosam dolores provident nostrum est facere, veniam, ipsam illo voluptatem perspiciatis recusandae id.</p>
+            <h1>{props.title}</h1>
+            <p>{props.content}</p>
         </div>
     );
 }
