49df271a7610ec98bd51c95c965283f49a48e915
EvanAristy <evanjaristy@gmail.com>
2022-05-09 20:24:58 -0400

    need to fix the css

--- src/components/CreateArea/index.jsx
+++ src/components/CreateArea/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-const CreateArea = () => {
+const CreateArea = (props) => {
     const [note, setNote] = useState({
         title: "",
         content: ""
