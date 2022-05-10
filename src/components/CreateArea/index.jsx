702508a9bf7b242ea432a1f1ffa94c5032a14b45
EvanAristy <evanjaristy@gmail.com>
2022-05-09 20:16:13 -0400

    added the hadle change and addNote functionality

--- src/components/CreateArea/index.jsx
+++ src/components/CreateArea/index.jsx
@@ -1,11 +1,51 @@
-import React from 'react';
+import React, { useState } from "react";
 
 const CreateArea = () => {
+    const [note, setNote] = useState({
+        title: "",
+        content: ""
+    })
+
+    function handleChange(e) {
+        const { name, value } = e.target;
+
+        setNote((prevNote) => {
+            return {
+                ...prevNote,
+                [name]: value
+            }
+        })
+    }
+
+    function submitNote(e) {
+        e.preventDefault()
+        props.onAdd(note)
+        setNote({
+            title: "",
+            content: ""
+        })
+    }
+
     return (
         <div>
-            
+            <form>
+                <input
+                    name="title"
+                    onChange={handleChange}
+                    value={note.title}
+                    placeholder="Title"
+                />
+                <textarea
+                    name="content"
+                    onChange={handleChange}
+                    value={note.content}
+                    placeholder="Take a note..."
+                    rows="3"
+                />
+                <button onClick={submitNote}>Add</button>
+            </form>
         </div>
     );
-}
+};
 
 export default CreateArea;
