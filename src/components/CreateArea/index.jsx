8148858624e59f4a7e756ff2ce7335bec10c04e6
EvanAristy <evanjaristy@gmail.com>
2022-05-11 12:51:27 -0400

    installed mui package and added a delete icon for the delete button

--- src/components/CreateArea/index.jsx
+++ src/components/CreateArea/index.jsx
@@ -28,7 +28,7 @@ const CreateArea = (props) => {
 
     return (
         <div>
-            <form>
+            <form className="create-note">
                 <input
                     name="title"
                     onChange={handleChange}
